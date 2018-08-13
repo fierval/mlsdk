5e5067189c2a580e4a61131f28377f4758547a29
fierval <fierval@gmail.com>
2018-08-12 22:16:35 -0700

    removing densecrf

--- iuml/training/__init__.py
+++ iuml/training/__init__.py
@@ -4,4 +4,3 @@ from __future__ import absolute_import
 from .train import TrainClassifierBase
 from .train_inception_base import TrainClassifierInceptionBase
 from .classifiers import TrainClassifierVgg16, TrainClassifierInceptionV3, TrainClassifierXception, TrainClassifierResnet50
-from .crf import get_dcrf_mask
